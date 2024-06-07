c5998c43664de94faceba243313d55cf3aee8cb1
SreekarMani <sreekar.army@gmail.com>
2024-06-07 20:29:00 +0530

    day 1 7-6-2024 started and setup git gave a introduction

---
+++ CODENOTES python CBSE .py
@@ -0,0 +1,58 @@
+'''
+
+
+
+
+'''
+
+
+
+
+
+
+
+
+'''
+7-6-2024
+
+
+
+INTRODUCTION
+
+
+Python - Guido Van Rossum in early 1990 
+
+TOKENS:
+
+smallest individual unit in a program => Token / Lexical Unit
+
+Tokens:-
+
+i. Keywords    : predefined words with special meaning for complier or interpreter - and, continue, if, elif, pass, raise, yield, finally, assert, is, in, not, or, with,
+
+ii. Identifiers (Names)   : User defined,
+
+ must not be a keyword, no spaces in between words, only letters numbers and underscore "_" , it can contain numbers but cannot begin with it
+
+iii. Literals  :  data items that have fixed value 
+
+1. String Literals
+
+'''
+
+text1="Hello world"  # single line string
+text2="hello\
+    world "          # multi line string with backslash
+text3='''
+
+hello                 
+world
+
+'''                   # multi line string with out backslash
+
+# Escape Sequences 
+
+print("\\ \' \" ")  # these elements cannot be typed without \ as they also similar to keywords
+
+print("\n \t \r  ")  # \n for new line \t for horizontal tab \r carriage return
+
